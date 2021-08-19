5ff79896c40a70b6e2d6d34d01361b5750f23771
Mihir Ravi <mihirravi500@gmail.com>
2021-08-19 13:23:08 +0530

    Setting up DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connString = process.env.DATABASE_CONNECTION;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(connString, {
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
+  try {
+    await mongoose.connect(connString, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
-        console.log('MongoDB connection SUCCESS');
-    } catch (error) {
-        console.log('MongoDB connection FAIL');
-        console.log(error);
-        process.exit(1);
-    }
-}
+    console.log("MongoDB connection SUCCESS");
+  } catch (error) {
+    console.log("MongoDB connection FAIL");
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
